6e0f1fa85812e0beee9d2e45204bea473c583b62
Petr Škoda <skodapetr@gmail.com>
2019-10-04 11:43:57 +0200

    Odstranění odkazů z navbaru
    #7

--- client/job-position/detail/index.js
+++ client/job-position/detail/index.js
@@ -4,10 +4,10 @@ import {createStore, STORE_NAME} from "../job-position-store";
 
 const spec = {
   "route-name": "job-position-detail",
-  "route": "/job-positions/:code",
+  "route": "/:code",
   "component": JobPositionDetail,
   "store-name": STORE_NAME,
-  "store-factory": createStore
+  "store-factory": createStore,
 };
 
 register(spec);
--- client/job-position/list/index.js
+++ client/job-position/list/index.js
@@ -4,12 +4,10 @@ import {createStore, STORE_NAME} from "../job-position-store";
 
 const spec = {
   "route-name": "job-position-list",
-  "route": "/job-positions",
+  "route": "/",
   "component": JobPositionList,
   "store-name": STORE_NAME,
   "store-factory": createStore,
-  "nav": true,
-  "nav-label": "Seznam inzerátů"
 };
 
 register(spec);
